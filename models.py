c32b4116cbe95ec7b5ef252013ae969bd1704605
matteopolleschi <matteo.polleschi@gmail.com>
2018-08-11 20:56:24 +0200

    Update models.py

--- models.py
+++ models.py
@@ -7,3 +7,8 @@ class res_company(models.Model):
     _inherit = 'res.company'
 
     tuco_id = fields.Integer('Tuco ID')
+
+class res_partner(models.Model):
+    _inherit = 'res.partner'
+
+    tuco_id = fields.Integer('Tuco ID')
