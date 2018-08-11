dd5008c0ba03285f8ffcb3fcb11d708d6f5f6e75
matteopolleschi <matteo.polleschi@gmail.com>
2018-08-11 20:31:32 +0200

    Create __openerp__.py

---
+++ __openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo for Publishing",
+
+    'summary': "Module for publishing businesses",
+
+    'description': "Additional fields and VAT mapping",
+
+    'author': "Daphne Solutions",
+    'website': "http://www.github.com/matteopolleschi/odoo_imppn",
+    'category': 'Accounting & Finance',
+    'version': '0.1',
+
+    'depends': ['web','base','account_accountant'],
+
+    'data': [
+        'views.xml',
+    ],
+}
