24d9e84b9d9db655a9423ff9794c6845c10eebfa
matteopolleschi <matteo.polleschi@gmail.com>
2018-08-11 20:35:16 +0200

    Update models.py

--- models.py
+++ models.py
@@ -6,4 +6,4 @@ from openerp import models, fields, api
 class res_company(models.Model):
     _inherit = 'res.company'
 
-tuco_id = fields.Integer('Tuco ID')
+    tuco_id = fields.Integer('Tuco ID')
