f813e31fe4d2876d570895cea75ce81cab3dcc2f
Mounir LAHSINI <mounirlahsini.ml@gmail.com>
2018-08-21 23:47:01 +0100

    update for odoo11

--- __manifest__.py
+++ __manifest__.py
@@ -11,7 +11,7 @@
     'category': 'Accounting & Finance',
     'version': '0.1',
 
-    'depends': ['web','base','account_accountant'],
+    'depends': ['web','base','account_invoicing'],
 
     'data': [
         'views.xml',
